6b72fa7427ea9154fcc5ada16829f95b6c9cbdfb
Pablo Pérez Núñez <pabloperez@uniovi.es>
2024-06-05 19:32:33 +0200

    Mejoras de la interfaz

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 document.addEventListener('DOMContentLoaded', async () => {
+    let trainWorker;
+    let consola = document.getElementById('console');
+
+    // Función para la console
+    const console_log = (line) => {
+        consola.textContent = consola.textContent+"\n"+line;
+        consola.scrollTop = consola.scrollHeight;
+    };
+
+
+    const btnStart = document.getElementById('btn-start');
+    const btnStop = document.getElementById('btn-stop');
+    
     // Comprobar si WebGL está disponible
     if (tf.ENV.get('WEBGL_VERSION') > 0) {
         console.log('WebGL is enabled');
@@ -7,63 +20,118 @@ document.addEventListener('DOMContentLoaded', async () => {
     }
 
     const statusDiv = document.getElementById('status');
-    const progressBar = document.getElementsByClassName('progress-bar')[0] //.getElementById('progress-bar');
+    const progressBar = document.getElementsByClassName('progress-bar')[0]
     
-    // Cargar los datos
-    const response = await fetch('ratings.json');
-    const ratings = await response.json();
+    // Cargar los datos de entrenamiento
+    console_log("Cargando los datos de entrenamiento...");
+    const trainResponse = await fetch('ratings_train.json');
+    const trainRatings = await trainResponse.json();
 
-    // Preparar los datos
-    const userIds = [...new Set(ratings.map(r => r.userId))];
-    const movieIds = [...new Set(ratings.map(r => r.movieId))];
+    // Cargar los datos de validación
+    console_log("Cargando los datos de validación...");
+    const valResponse = await fetch('ratings_val.json');
+    const valRatings = await valResponse.json();
 
-    const numUsers = userIds.length;
-    const numMovies = movieIds.length;
+    // Habilitar los botones una vez que los datos se han cargado
+    btnStart.disabled = false;
 
-    const userIdToIndex = Object.fromEntries(userIds.map((id, index) => [id, index]));
-    const movieIdToIndex = Object.fromEntries(movieIds.map((id, index) => [id, index]));
+    // Preparar los datos de entrenamiento
+    console_log("Preprocesando datos...");
 
-    const userInputs = ratings.map(r => userIdToIndex[r.userId]);
-    const movieInputs = ratings.map(r => movieIdToIndex[r.movieId]);
-    const outputs = ratings.map(r => r.rating);
+    const trainUserIds = [...new Set(trainRatings.map(r => r.userId))];
+    const trainMovieIds = [...new Set(trainRatings.map(r => r.movieId))];
 
-    // Crear un nuevo Web Worker para el entrenamiento del modelo
-    const trainWorker = new Worker('train-worker.js');
+    const trainUserIdToIndex = Object.fromEntries(trainUserIds.map((id, index) => [id, index]));
+    const trainMovieIdToIndex = Object.fromEntries(trainMovieIds.map((id, index) => [id, index]));
+
+    const trainUserInputs = trainRatings.map(r => trainUserIdToIndex[r.userId]);
+    const trainMovieInputs = trainRatings.map(r => trainMovieIdToIndex[r.movieId]);
+    const trainOutputs = trainRatings.map(r => r.rating);
+
+    // Preparar los datos de validación
+    const valUserIds = [...new Set(valRatings.map(r => r.userId))];
+    const valMovieIds = [...new Set(valRatings.map(r => r.movieId))];
+
+    const valUserIdToIndex = Object.fromEntries(valUserIds.map((id, index) => [id, index]));
+    const valMovieIdToIndex = Object.fromEntries(valMovieIds.map((id, index) => [id, index]));
+
+    const valUserInputs = valRatings.map(r => valUserIdToIndex[r.userId]);
+    const valMovieInputs = valRatings.map(r => valMovieIdToIndex[r.movieId]);
+    const valOutputs = valRatings.map(r => r.rating);
+
+    const numUsers = Math.max(trainUserIds.length, valUserIds.length);
+    const numMovies = Math.max(trainMovieIds.length, valMovieIds.length);
 
     let embeddingsPlotCreated = false;
     let lossPlotCreated = false;
+    
+    // Función para iniciar el entrenamiento del modelo en el Web Worker
+    const startTraining = () => {
+        console_log("Iniciando entrenamiento...");
 
-    // Escuchar los mensajes del Web Worker para el entrenamiento
-    trainWorker.addEventListener('message', (event) => {
-        const { type, status, progress, lossHistory, userEmbeddings, movieEmbeddings } = event.data;
-        if (type === 'status') {
-            statusDiv.textContent = status;
-        } else if (type === 'progress') {
-            progressBar.value = progress;
-            progressBar.style.width = `${progress}%`;
-        } else if (type === 'plot') {
-            if (!embeddingsPlotCreated) {
-                createEmbeddingsPlot(userEmbeddings, movieEmbeddings, numUsers, numMovies);
-                embeddingsPlotCreated = true;
-            } else {
-                updateEmbeddingsPlot(userEmbeddings, movieEmbeddings);
+        trainWorker = new Worker('train-worker.js');
+        
+        // Deshabilitar btn-start y habilitar btn-stop
+        btnStart.disabled = true;
+        btnStop.disabled = false;
+
+        // Escuchar los mensajes del Web Worker para el entrenamiento
+        trainWorker.addEventListener('message', (event) => {
+            const { type, status, progress, lossHistory, valLossHistory, userEmbeddings, movieEmbeddings } = event.data;
+            if (type === 'status') {
+                console_log(status);
+            } else if (type === 'progress') {
+                progressBar.value = progress;
+                progressBar.style.width = `${progress}%`;
+            } else if (type === 'plot') {
+                if (!embeddingsPlotCreated) {
+                    createEmbeddingsPlot(userEmbeddings, movieEmbeddings, numUsers, numMovies);
+                    embeddingsPlotCreated = true;
+                } else {
+                    updateEmbeddingsPlot(userEmbeddings, movieEmbeddings);
+                }
+                if (!lossPlotCreated) {
+                    createLossPlot(lossHistory, valLossHistory);
+                    lossPlotCreated = true;
+                } else {
+                    updateLossPlot(lossHistory, valLossHistory);
+                }
             }
-            if (!lossPlotCreated) {
-                createLossPlot(lossHistory);
-                lossPlotCreated = true;
-            } else {
-                updateLossPlot(lossHistory);
-            }
+        });
+
+        // Iniciar el entrenamiento del modelo en el Web Worker
+        trainWorker.postMessage({ 
+            trainUserInputs, trainMovieInputs, trainOutputs, 
+            valUserInputs, valMovieInputs, valOutputs, 
+            numUsers, numMovies 
+        });
+    };
+
+    // Función para detener el entrenamiento del modelo en el Web Worker
+    const stopTraining = () => {
+        console_log("Entrenamiento detenido");
+
+        if (trainWorker) {
+            trainWorker.terminate();
+            statusDiv.textContent = 'Training stopped';
+            progressBar.value = 0;
+            progressBar.style.width = '0%';
+            trainWorker = null;
+
+            // Habilitar btn-start y deshabilitar btn-stop
+            btnStart.disabled = false;
+            btnStop.disabled = true;
         }
-    });
+    };
 
-    // Iniciar el entrenamiento del modelo en el Web Worker
-    trainWorker.postMessage({ userInputs, movieInputs, outputs, numUsers, numMovies });
+    // Event listeners para los botones
+    btnStart.addEventListener('click', startTraining);
+    btnStop.addEventListener('click', stopTraining);
 
     // Función para visualizar embeddings
     const createEmbeddingsPlot = async (userEmbeddings, movieEmbeddings, numUsers, numMovies) => {
-        const userCoords = Array.from({length: numUsers}, (_, i) => userEmbeddings[i]);
-        const movieCoords = Array.from({length: numMovies}, (_, i) => movieEmbeddings[i]);
+        const userCoords = Array.from({ length: numUsers }, (_, i) => userEmbeddings[i]);
+        const movieCoords = Array.from({ length: numMovies }, (_, i) => movieEmbeddings[i]);
 
         const userTrace = {
             x: userCoords.map(coord => coord[0]),
@@ -84,40 +152,53 @@ document.addEventListener('DOMContentLoaded', async () => {
         };
 
         const data = [userTrace, movieTrace];
-        var layout = { 
-            margin: {t:50, l:50, r:50, b:50},
+        var layout = {
+            margin: { t: 50, l: 50, r: 50, b: 50 },
             legend: { x: 1, xanchor: 'right', y: 1 }
         };
 
-        Plotly.newPlot('emb-plot', data, layout, {responsive: true});
+        Plotly.newPlot('emb-plot', data, layout, { responsive: true });
     };
 
     const updateEmbeddingsPlot = async (userEmbeddings, movieEmbeddings) => {
-        const userCoords = Array.from({length: userEmbeddings.length}, (_, i) => userEmbeddings[i]);
-        const movieCoords = Array.from({length: movieEmbeddings.length}, (_, i) => movieEmbeddings[i]);
+        const userCoords = Array.from({ length: userEmbeddings.length }, (_, i) => userEmbeddings[i]);
+        const movieCoords = Array.from({ length: movieEmbeddings.length }, (_, i) => movieEmbeddings[i]);
 
         Plotly.restyle('emb-plot', 'x', [userCoords.map(coord => coord[0]), movieCoords.map(coord => coord[0])]);
         Plotly.restyle('emb-plot', 'y', [userCoords.map(coord => coord[1]), movieCoords.map(coord => coord[1])]);
     };
 
-    const createLossPlot = (lossHistory) => {
-        const trace = {
+    const createLossPlot = (lossHistory, valLossHistory) => {
+        const trainTrace = {
             x: lossHistory.map((_, i) => i + 1),
             y: lossHistory,
             type: 'scatter',
             mode: 'lines+markers',
-            name: 'Loss',
+            name: 'Train Loss',
+            line: { color: 'blue' }
         };
 
-        var layout = { margin: {t:50, l:50, r:50, b:50} };
-        const data = [trace];
-        Plotly.newPlot('loss-plot', data, layout, {responsive: true});
+        const valTrace = {
+            x: valLossHistory.map((_, i) => i + 1),
+            y: valLossHistory,
+            type: 'scatter',
+            mode: 'lines+markers',
+            name: 'Validation Loss',
+            line: { color: 'red' }
+        };
+
+        var layout = {
+            margin: { t: 50, l: 50, r: 50, b: 50 },
+            legend: { x: 1, xanchor: 'right', y: 1 }
+        };
+        
+        const data = [trainTrace, valTrace];
+        Plotly.newPlot('loss-plot', data, layout, { responsive: true });
     };
 
-    const updateLossPlot = (lossHistory) => {
-        Plotly.restyle('loss-plot', 'x', [lossHistory.map((_, i) => i + 1)]);
-        Plotly.restyle('loss-plot', 'y', [lossHistory]);
+    const updateLossPlot = (lossHistory, valLossHistory) => {
+        Plotly.restyle('loss-plot', 'x', [lossHistory.map((_, i) => i + 1), valLossHistory.map((_, i) => i + 1)]);
+        Plotly.restyle('loss-plot', 'y', [lossHistory, valLossHistory]);
     };
-
 
 });
--- train-worker.js
+++ train-worker.js
@@ -1,20 +1,23 @@
-// train-worker.js
-
 importScripts('https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@3.12.0/dist/tf.min.js');
 
-async function trainModel(userInputs, movieInputs, outputs, numUsers, numMovies, statusCallback, progressBarCallback) {
+async function trainModel(trainUserInputs, trainMovieInputs, trainOutputs, valUserInputs, valMovieInputs, valOutputs, numUsers, numMovies, statusCallback, progressBarCallback) {
     
     // Preparar los datos de entrenamiento
-    const userTensor = tf.tensor2d(userInputs, [userInputs.length, 1]);
-    const movieTensor = tf.tensor2d(movieInputs, [movieInputs.length, 1]);
-    const ratingsTensor = tf.tensor1d(outputs);
+    const trainUserTensor = tf.tensor2d(trainUserInputs, [trainUserInputs.length, 1]);
+    const trainMovieTensor = tf.tensor2d(trainMovieInputs, [trainMovieInputs.length, 1]);
+    const trainRatingsTensor = tf.tensor1d(trainOutputs);
+
+    // Preparar los datos de validación
+    const valUserTensor = tf.tensor2d(valUserInputs, [valUserInputs.length, 1]);
+    const valMovieTensor = tf.tensor2d(valMovieInputs, [valMovieInputs.length, 1]);
+    const valRatingsTensor = tf.tensor1d(valOutputs);
     
     // Crear el modelo de factorización de matrices
-    const userInput = tf.input({shape: [1], name: 'userInput'});
-    const movieInput = tf.input({shape: [1], name: 'movieInput'});
+    const userInput = tf.input({ shape: [1], name: 'userInput' });
+    const movieInput = tf.input({ shape: [1], name: 'movieInput' });
 
-    const userEmbeddingLayer = tf.layers.embedding({inputDim: numUsers, outputDim: 2, inputLength: 1});
-    const movieEmbeddingLayer = tf.layers.embedding({inputDim: numMovies, outputDim: 2, inputLength: 1});
+    const userEmbeddingLayer = tf.layers.embedding({ inputDim: numUsers, outputDim: 2, inputLength: 1 });
+    const movieEmbeddingLayer = tf.layers.embedding({ inputDim: numMovies, outputDim: 2, inputLength: 1 });
 
     const userEmbedding = userEmbeddingLayer.apply(userInput);
     const movieEmbedding = movieEmbeddingLayer.apply(movieInput);
@@ -23,19 +26,19 @@ async function trainModel(userInputs, movieInputs, outputs, numUsers, numMovies,
     const movieVector = tf.layers.flatten().apply(movieEmbedding);
 
     // Añadir bias a las embeddings
-    const userBias = tf.layers.embedding({inputDim: numUsers, outputDim: 1, inputLength: 1}).apply(userInput);
-    const movieBias = tf.layers.embedding({inputDim: numMovies, outputDim: 1, inputLength: 1}).apply(movieInput);
+    const userBias = tf.layers.embedding({ inputDim: numUsers, outputDim: 1, inputLength: 1 }).apply(userInput);
+    const movieBias = tf.layers.embedding({ inputDim: numMovies, outputDim: 1, inputLength: 1 }).apply(movieInput);
 
     const userBiasVector = tf.layers.flatten().apply(userBias);
     const movieBiasVector = tf.layers.flatten().apply(movieBias);
 
     // Calcular el dot product y añadir bias
-    const dotProduct = tf.layers.dot({axes: -1}).apply([userVector, movieVector]);
+    const dotProduct = tf.layers.dot({ axes: -1 }).apply([userVector, movieVector]);
 
     // Sumar los biases
     const addBias = tf.layers.add().apply([dotProduct, userBiasVector, movieBiasVector]);
 
-    const scaledOutput = tf.layers.activation({activation: 'relu'}).apply(addBias);
+    const scaledOutput = tf.layers.activation({ activation: 'relu' }).apply(addBias);
 
     const model = tf.model({
         inputs: [userInput, movieInput],
@@ -50,34 +53,41 @@ async function trainModel(userInputs, movieInputs, outputs, numUsers, numMovies,
     // Entrenar el modelo
     const epochs = 100;
     const batchSize = 128;
-    const totalBatches = Math.ceil(userInputs.length / batchSize) * epochs;
+    const totalBatches = Math.ceil(trainUserInputs.length / batchSize) * epochs;
 
     let batchCount = 0;
     let epochCount = 0;
     let last_loss = null;
+    let last_val_loss = null;
     const lossHistory = [];
+    const valLossHistory = [];
 
-    await model.fit([userTensor, movieTensor], ratingsTensor, {
+    await model.fit([trainUserTensor, trainMovieTensor], trainRatingsTensor, {
         epochs: epochs,
         batchSize: batchSize,
+        validationData: [[valUserTensor, valMovieTensor], valRatingsTensor],
         callbacks: {
             onBatchEnd: async (batch, logs) => {
                 batchCount++;
                 const progress = (batchCount / totalBatches) * 100;
                 progressBarCallback(progress);
-                if(last_loss == null){ last_loss = logs.loss.toFixed(4) }
-                statusCallback(`Batch ${batch + 1}, Epoch ${epochCount + 1}: loss = ${last_loss}`);
+                if (last_loss == null) { last_loss = logs.loss.toFixed(4); }
             },
             onEpochEnd: async (epoch, logs) => { 
                 epochCount++;
-                last_loss = logs.loss.toFixed(4)
+                last_loss = logs.loss.toFixed(4);
+                last_val_loss = logs.val_loss.toFixed(4);
                 lossHistory.push(last_loss);
+                valLossHistory.push(last_val_loss);
                 const userEmbeddings = await userEmbeddingLayer.getWeights()[0].array();
                 const movieEmbeddings = await movieEmbeddingLayer.getWeights()[0].array();
-                self.postMessage({ type: 'plot', lossHistory, userEmbeddings, movieEmbeddings});
+                self.postMessage({ type: 'plot', lossHistory, valLossHistory, userEmbeddings, movieEmbeddings });
+
+                statusCallback(`[Epoch ${epochCount}] Train_loss: ${last_loss}, Val_loss: ${last_val_loss}`);
+
             },
             onTrainEnd: () => {
-                statusCallback('Training completed!');
+                statusCallback('Entrenamiento completado!');
                 progressBarCallback(100);
             }
         }
@@ -85,8 +95,8 @@ async function trainModel(userInputs, movieInputs, outputs, numUsers, numMovies,
 }
 
 self.addEventListener('message', async (event) => {
-    const { userInputs, movieInputs, outputs, numUsers, numMovies } = event.data;
-    await trainModel(userInputs, movieInputs, outputs, numUsers, numMovies, (status) => {
+    const { trainUserInputs, trainMovieInputs, trainOutputs, valUserInputs, valMovieInputs, valOutputs, numUsers, numMovies } = event.data;
+    await trainModel(trainUserInputs, trainMovieInputs, trainOutputs, valUserInputs, valMovieInputs, valOutputs, numUsers, numMovies, (status) => {
         self.postMessage({ type: 'status', status });
     }, (progress) => {
         self.postMessage({ type: 'progress', progress });
